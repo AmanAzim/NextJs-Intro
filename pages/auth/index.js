8d982bd32e98a8a90cceb4ce51e4272b16a06cb4
Aman <rumman.djuice1@gmail.com>
2019-03-31 21:04:40 +0200

    "commit"

---
+++ pages/auth/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import User from '../../components/User'
+
+const authindexPage=()=>{
+    return(
+        <div>
+            <h1>The Auth Index page</h1>
+            <User name="Aman" age="31"/>
+        </div>
+    )
+};
+
+export default authindexPage;
